45ed0b09e85ddb3df90ce73aa3ddf818cdf6f719
19leunam93 <tech.spuur@quickline.ch>
2026-06-21 14:43:40 +0200

    fix version

--- services/provider.php
+++ services/provider.php
@@ -1,10 +1,10 @@
 <?php
 /**
 ****************************************************************************
-**   @version    2.0.0                                                    **
+**   @version    2.1.0                                                    **
 **   @package    mod_tinyslider                                           **
 **   @author     Manuel Häusler <tech.spuur@quickline.ch>                 **
-**   @copyright  2024 Manuel Haeusler                                     **
+**   @copyright  2026 Manuel Haeusler                                     **
 **   @license    GNU General Public License version 3 or later            **
 ****************************************************************************/
 
@@ -18,19 +18,19 @@ use Joomla\DI\ServiceProviderInterface;
 
 return new class implements ServiceProviderInterface
 {
-    /**
-     * Registers the service provider with a DI container.
-     *
-     * @param   Container  $container  The DI container.
-     *
-     * @return  void
-     *
-     * @since   2.0.0
-     */
-    public function register(Container $container): void
-    {
-        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Elfangor93\\Module\\Tinyslider'));
-        $container->registerServiceProvider(new HelperFactory('\\Elfangor93\\Module\\Tinyslider\\Site\\Helper'));
-        $container->registerServiceProvider(new Module());
-    }
+  /**
+   * Registers the service provider with a DI container.
+   *
+   * @param   Container  $container  The DI container.
+   *
+   * @return  void
+   *
+   * @since   2.0.0
+   */
+  public function register(Container $container): void
+  {
+    $container->registerServiceProvider(new ModuleDispatcherFactory('\\Elfangor93\\Module\\Tinyslider'));
+    $container->registerServiceProvider(new HelperFactory('\\Elfangor93\\Module\\Tinyslider\\Site\\Helper'));
+    $container->registerServiceProvider(new Module());
+  }
 };
